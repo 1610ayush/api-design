1470f101fe2abe076e094bc22f0e6fce89df054d
1610ayush <ayushranjan.iit@gmail.com>
2022-09-13 13:43:57 +0530

    add all files of api-design

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+
+export const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+const log = (req, res, next) => {
+    console.log("logging")
+    next()
+}
+
+app.get('/data', log, (req, res) => {
+    res.send({ data: [1, 2, 3] })
+})
+
+app.post("/data", (req, res) => {
+    console.log(req.body)
+    res.send({ ok: true })
+})
+
+export const start = () => {
+    app.listen(4500, () => {
+        console.log("server is on 4500")
+    })
+}
